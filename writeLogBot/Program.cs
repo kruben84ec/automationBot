5e50e3d87c15a1c71f91d0bd403c2647a78c6fb9
cmiranda4 <cmiranda@auconsis.com.ec>
2021-12-19 10:27:36 -0500

    Valdiación de parametros de entrada

--- writeLogBot/Program.cs
+++ writeLogBot/Program.cs
@@ -10,18 +10,26 @@ namespace writeLogBot
             string pathFile = "";
             string logWrite = "";
             string messageLog = "";
+            //Obtener la hora del sistema
             var dateTime = DateTime.Now;
             var dateLogWrite = dateTime.ToString("yyyy-MM-dd hh:mm:ss");
 
             if (args.Length != 0)
             {
+                //Path donde se va reposar el archivo de Log
                 pathFile = args[0];
                 messageLog = args[1];
-                using (StreamWriter sw = File.AppendText(pathFile))
+
+                if(pathFile!="" && messageLog!="") {
+                    using (StreamWriter sw = File.AppendText(pathFile)) {
+                        logWrite += dateLogWrite + ";";
+                        logWrite += messageLog + ";";
+                        sw.WriteLine(logWrite);
+                    }
+                }
+                else
                 {
-                    logWrite += dateLogWrite + ";";
-                    logWrite += messageLog + ";";
-                    sw.WriteLine(logWrite);
+                    Console.WriteLine("Los argumentos no sson validos");
                 }
             }
 
